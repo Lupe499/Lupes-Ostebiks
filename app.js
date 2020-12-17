dfcb7cc578eb1536e145ca0c5a2f2b603c4193ae
Lucas Pedersen <lucas.a.pedersen@gmail.com>
2020-12-17 14:20:24 +0100

    looking ok if i do say so my self

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-fetch("https://osteapi-lupe.herokuapp.com/api/v1/cheeses")
-.then(res => res.json())
-.then(function(data) {
+let url = new URLSearchParams(window.location.search);
+
+let offset = url.get("offset") ? url.get("offset") : 0;
+let prevOffset, nextOffset;
+var next = document.querySelector(".next")
+var prev = document.querySelector(".prev")
+
+fetch(`https://osteapi-lupe.herokuapp.com/api/v1/cheeses?offset=${offset}`)
+    .then(response => response.json())
+    .then(function (data){
+        let maxOffset = data.count - (data.count % 5);
+
+        nextOffset = offset >= maxOffset ? maxOffset : parseInt(offset) + 5;
+        prevOffset = offset <= 0 ? 0 : parseInt(offset) - 5;
+        next.href = `?offset=${nextOffset}`;
+        prev.href = `?offset=${prevOffset}`;
+        document.querySelector(".count").innerHTML=`${data.count}`
+
+        
     console.log(data);
-})
+    var osteliste = document.querySelector(".osteliste")
+    data.results.forEach(result => {
+        console.log(result);
+        var oste = document.createElement("div");
+        oste.classList.add("ost");
+        oste.innerHTML=`
+            <p class="name">${result.name}</p>
+            <p class="price">${result.price.$numberDecimal} kr</p>
+            <p class="strength">${result.strength}</p>
+            <p class="brand">${result.brand}</p>
+        `
+        osteliste.appendChild(oste);
+    });
+}); 
