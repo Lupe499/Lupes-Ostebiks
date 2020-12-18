e746e5bdaab6c9f7a32899bc1428524a84ed55d7
Lucas Pedersen <lucas.a.pedersen@gmail.com>
2020-12-18 11:10:36 +0100

    possebly the last commit

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ let url = new URLSearchParams(window.location.search);
 
 let offset = url.get("offset") ? url.get("offset") : 0;
 let prevOffset, nextOffset;
-var next = document.querySelector(".next")
-var prev = document.querySelector(".prev")
+var next = document.querySelector(".next");
+var prev = document.querySelector(".prev");
+var page = document.querySelector(".pageNumber");
 
 fetch(`https://osteapi-lupe.herokuapp.com/api/v1/cheeses?offset=${offset}`)
     .then(response => response.json())
@@ -12,20 +13,26 @@ fetch(`https://osteapi-lupe.herokuapp.com/api/v1/cheeses?offset=${offset}`)
 
         nextOffset = offset >= maxOffset ? maxOffset : parseInt(offset) + 5;
         prevOffset = offset <= 0 ? 0 : parseInt(offset) - 5;
+
         next.href = `?offset=${nextOffset}`;
         prev.href = `?offset=${prevOffset}`;
+
         document.querySelector(".count").innerHTML=`${data.count}`
 
+        if(offset == 0 || null){
+            page.innerHTML= "1";
+        }else{
+            page.innerHTML=`${offset / 5 + 1}`
+        }
         
-    console.log(data);
     var osteliste = document.querySelector(".osteliste")
     data.results.forEach(result => {
-        console.log(result);
         var oste = document.createElement("div");
         oste.classList.add("ost");
         oste.innerHTML=`
             <p class="name">${result.name}</p>
             <p class="price">${result.price.$numberDecimal} kr</p>
+            <p class="weight">${result.weight} g</p>
             <p class="strength">${result.strength}</p>
             <p class="brand">${result.brand}</p>
         `
